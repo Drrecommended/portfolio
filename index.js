45e29387adf261308fca95c66466206fae807aae
Tyler French <tylerfrench89@gmail.com>
2021-02-22 11:51:43 -0800

    fixed navbar issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("nav-links")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navbarButtons = document.querySelectorAll(".nav-button");
 
 toggleButton.addEventListener("click", () => {
