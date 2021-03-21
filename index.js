55b2ab7df3ec7dd8c00e98d0e83eaec2083b5927
Tyler French <tylerfrench89@gmail.com>
2021-03-20 18:18:30 -0700

    working on scroll animations

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+const scrollOffset = 100;
+
+const scrollElement = document.querySelector(".js-scroll");
+
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) - offset
+  );
+};
+
+const displayScrollElement = () => {
+  scrollElement.classList.add("scrolled");
+};
+
+const hideScrollElement = () => {
+  scrollElement.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+    displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
+
+//navbar
+
 function navBar() {
   $(".navbar-nav>li>a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
+    if ($("boot-button").attr("aria-expanded") === "true") {
+      console.log("it's true my nig");
+    }
   });
-  if ($(".navbar-collapse").attr("show") === "true") {
-      console.log("it's true my nig")
-  }
 }
 
 navBar();
-
-
-let names = ['tim', 'kyle', '  cristina', 'connor  ']
-
-function cleanNames(arr) {
-  return arr.map((name) => {
-    console.log(name.trim())
-  })
-}
-
-cleanNames(names)
