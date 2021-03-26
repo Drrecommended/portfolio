34a2ccd76f01097a3086d745026b0bb330e0bf53
Tyler French <tylerfrench89@gmail.com>
2021-03-25 20:43:59 -0700

    finished navbar

--- index.js
+++ index.js
@@ -1,44 +1,8 @@
-const scrollOffset = 100;
-
-const scrollElement = document.querySelector(".js-scroll");
-
-const elementInView = (el, offset = 0) => {
-  const elementTop = el.getBoundingClientRect().top;
-
-  return (
-    elementTop <=
-    (window.innerHeight || document.documentElement.clientHeight) - offset
-  );
-};
-
-const displayScrollElement = () => {
-  scrollElement.classList.add("scrolled");
-};
-
-const hideScrollElement = () => {
-  scrollElement.classList.remove("scrolled");
-};
-
-const handleScrollAnimation = () => {
-  if (elementInView(scrollElement, scrollOffset)) {
-    displayScrollElement();
-  } else {
-    hideScrollElement();
-  }
-};
-
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
-});
-
 //navbar
 
 function navBar() {
   $(".navbar-nav>li>a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
-    if ($("boot-button").attr("aria-expanded") === "true") {
-      console.log("it's true my nig");
-    }
   });
 }
 
