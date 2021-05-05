ff74127e5ed6dabb000480f59fa31c29b6a96b7e
Tyler French <tylerfrench89@gmail.com>
2021-05-04 19:54:16 -0700

    connected form

--- index.js
+++ index.js
@@ -8,26 +8,3 @@ function navBar() {
 
 navBar();
 
-
-//form
-
-const contactForm = document.querySelector('#contactForm')
-contactForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-  console.log('SUBMITTED!')
-})
-
-
-
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault()
-//   const productName = document.getElementById('product')
-//   const productQuantity = document.getElementById('qty').value
-//   addToList(productName.value, productQuantity.value)
-// })
-
-// const addToList = (product, quantity) => {
-//   const newListItem = document.createElement('li')
-//   const listContainer = document.querySelector('#list')
-//   listContainer.append(newListItem)
-// }
